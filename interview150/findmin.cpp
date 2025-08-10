df75700a9b9590c406bca8163935711fd9b0090d
xiebaoma <2402583447@qq.com>
2025-08-10 17:36:21 +0800

    solve 2 problems, do nothing, get ready for job

---
+++ interview150/findmin.cpp
@@ -0,0 +1,30 @@
+/**
+ * xiebaoma
+ * 2025-08-10
+ */
+
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low < high)
+        {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high])
+            {
+                high = pivot;
+            }
+            else
+            {
+                low = pivot + 1;
+            }
+        }
+        return nums[low];
+    }
+};
