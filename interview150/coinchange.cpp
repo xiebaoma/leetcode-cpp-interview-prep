17c9b480d9310bb39419f6ec026d21e5585f0a9b
xiebaoma <2402583447@qq.com>
2025-08-14 23:53:55 +0800

    solve 2 problems , use smc to connect the server.

---
+++ interview150/coinchange.cpp
@@ -0,0 +1,24 @@
+/**
+ * xiebaoma
+ * 2025-08-14
+ */
+
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int Max = amount + 1;
+        vector<int> dp(amount + 1, Max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < (int)coins.size(); ++j) {
+                if (coins[j] <= i) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+};
