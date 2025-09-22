fd78443e9e85b79f1fb06def638591f57bceab64
xiebaoma <2402583447@qq.com>
2025-09-22 22:37:55 +0800

    solve 3 problems

---
+++ everyday/maxfrequenceelements.cpp
@@ -0,0 +1,38 @@
+/**
+ * xiebaoma
+ * 2025-09-22
+ */
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxFrequencyElements(vector<int> &nums)
+    {
+        unordered_map<int, int> count;
+        for (int a : nums)
+        {
+            count[a]++;
+        }
+        int maxf = 0;
+        for (auto const &pair : count)
+        {
+            if (pair.second > maxf)
+            {
+                maxf = pair.second;
+            }
+        }
+        int res = 0;
+        for (auto const &pair : count)
+        {
+            if (pair.second == maxf)
+            {
+                res += maxf;
+            }
+        }
+        return res;
+    }
+};
---
+++ leetcode_meta/577.sql
@@ -0,0 +1,7 @@
+# xiebaoma
+# 2025-09-22
+
+select name, bonus
+from Employee left join Bonus
+on Employee.EmpId = Bonus.EmpId
+where bonus is null or bonus < 1000
