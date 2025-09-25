0e295cdec7df03480ca028aa702efdf62e550dfd
xiebaoma <2402583447@qq.com>
2025-09-25 23:37:38 +0800

    solve 2 problems

---
+++ leetcode_bytedance/partionlabels.cpp
@@ -0,0 +1,30 @@
+/**
+ * xiebaoma
+ * 2025-09-25
+ */
+
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int last[26];
+        int length = s.size();
+        for (int i = 0; i < length; i++) {
+            last[s[i] - 'a'] = i;
+        }
+        vector<int> partition;
+        int start = 0, end = 0;
+        for (int i = 0; i < length; i++) {
+            end = max(end, last[s[i] - 'a']);
+            if (i == end) {
+                partition.push_back(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
+};
